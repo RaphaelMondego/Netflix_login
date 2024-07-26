457b17cd9a4f9fcda43c53342d1ef7079d9026bf
Raphael Mondego <raphamondego@gmail.com>
2024-07-26 15:35:35 -0300

    Adicionando o primeiro commit

---
+++ Landing_Page_Food/src/styles/javascript/script.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+    $('#mobile_btn').on('click', function(){
+        $('#mobile_menu').toggleClass('active')
+        $('#mobile_btn').find('i').toggleClass('fa-x')    //Botão menu superior do site: usando o jQuery acima para poder alterar a responsividade do site clicando no botão lá em cima para abrir o menu em dispositivos menores tipo celular.
+    })
+
+    const sections = $('section');
+    const navItems = $('.nav-item');
+
+    $(window).on('scroll', function(){
+        const header = $('header');
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
+
+        let activeSectionIndex = 0;
+
+        if(scrollPosition <= 0){
+            header.css('box-shadow', 'none');
+        } else {
+            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1');
+        }
+
+        sections.each(function(i){
+            const section = $(this);
+            const sectionTop = section.offset().top - 96;
+            const sectionBottom = sectionTop + section.outerHeight();
+
+            if(scrollPosition >= sectionTop && scrollPosition < sectionBottom){
+                activeSectionIndex = i;
+                return false;
+            }
+        })
+
+        navItems.removeClass('active')
+        $(navItems[activeSectionIndex]).addClass('active');
+    });
+
+
+    // ANIMANDO O SITE COM A BIBLIOTECA  DO SITE SCROLL REVEAL
+    ScrollReveal().reveal('#cta', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('.dish', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('#testimonial_chef', {
+        origin: 'left',
+        duration: 1000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('.feedback', {
+        origin: 'right',
+        duration: 1000,
+        distance: '20%'
+    });
+});   
+
